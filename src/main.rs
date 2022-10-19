d0a809148e7251231ee697415de1d58ec7933179
andmatcha <jj.mcr185@gmail.com>
2022-10-20 01:10:36 +0900

    feat: :sparkles: 入力を受け取り、そのまま出力

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("数を予想しよう！");
+
+    println!("キミの予想を入力してねん");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("行の読み込みに失敗しました");
+
+    println!("キミの予想: {}", guess);
 }
